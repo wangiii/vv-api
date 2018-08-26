68647e61298496f23ae19de8743370390efae313
wangiii <jasonwangiii@outlook.com>
2018-08-27 00:19:17 +0800

    fix VerificationCodesController

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -27,5 +27,14 @@ class VerificationCodesController extends Controller
                 return $this->response->errorInternal($message ?? '短信发送异常');
             }
         }
+        $key = 'verificationCode_'.str_random(15);
+        $expiredAt = now()->addMinutes(10);
+        // 缓存验证码 10分钟过期。
+        \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+
+        return $this->response->array([
+            'key' => $key,
+            'expired_at' => $expiredAt->toDateTimeString(),
+        ])->setStatusCode(201);
     }
 }
