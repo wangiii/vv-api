7f1fa33ebd68d628779b3d5e3a85e07cc8eec6ce
wangiii <jasonwangiii@outlook.com>
2018-09-08 23:40:36 +0800

    replies index

--- app/Http/Controllers/Api/RepliesController.php
+++ app/Http/Controllers/Api/RepliesController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\Topic;
 use App\Models\Reply;
+use App\Models\User;
 use App\Http\Requests\Api\ReplyRequest;
 use App\Transformers\ReplyTransformer;
+use App\Transformers\UserTransformer;
 
 class RepliesController extends Controller
 {
@@ -31,5 +33,19 @@ class RepliesController extends Controller
         $reply->delete();
 
         return $this->response->noContent();
+    }
+
+    public function index(Topic $topic)
+    {
+        $replies = $topic->replies()->paginate(20);
+
+        return $this->response->paginator($replies, new ReplyTransformer());
+    }
+
+    public function userIndex(User $user)
+    {
+        $replies = $user->replies()->paginate(20);
+
+        return $this->response->paginator($replies, new ReplyTransformer());
     }
 }
